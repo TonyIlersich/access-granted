aecfd4a3f9b13f2ad0a1f7502c32a49c3340d8ab
Anthony Ilersich <antonio.ilersich@gmail.com>
2020-05-04 18:30:54 -0400

    removed old styles and added styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import DefaultContainer from './Components/Containers/DefaultContainer';
+import DefaultText from './Components/Texts/DefaultText';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppContainer = styled(DefaultContainer)`
+  position: fixed;
+  margin: 0px;
+  border: 0px;
+  padding: 0px;
+  width: 100vw;
+  height: 100vh;
+`;
 
-export default App;
+export default () => (
+  <AppContainer>
+    <DefaultText>Hello World</DefaultText>
+  </AppContainer>
+);
---
+++ src/Components/Containers/DefaultContainer.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  margin: 0px;
+  border: 0px;
+  padding: 0px;
+  flex-grow: 1;
+`;
