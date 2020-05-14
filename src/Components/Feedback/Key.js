1208db31f9b2391f8e6ceb1fbee9a55ce9e9a168
Anthony Ilersich <antonio.ilersich@gmail.com>
2020-05-14 03:02:16 -0400

    made keyboard responsive

--- src/Components/Feedback/Key.js
+++ src/Components/Feedback/Key.js
@@ -6,23 +6,54 @@ import Margins from '../../Styles/Margins';
 import DefaultRow from '../Containers/DefaultRow';
 
 const Container = styled(DefaultRow)`
-    flex-grow: ${props => props.width || 1};
-    flex-shrink: ${props => props.width || 1};
-    flex-basis: 0;
-    border: 2px solid ${Colors.Cyan};
-    & ~ & {
-        margin-left: ${Margins.KeyMargin};
-    }
+  flex-grow: ${props => props.width || 1};
+  flex-shrink: ${props => props.width || 1};
+  background-color: ${props => props.bgColor || 'transparent'};
+  flex-basis: 0;
+  border: 2px solid ${Colors.Cyan};
+  & ~ & {
+    margin-left: ${Margins.KeyMargin};
+  }
 `;
 
 const Text = styled(SvgText)`
-    color: ${props => props.color || Colors.Cyan};
-    flex-grow: 1;
-    font-size: 100%;
+  color: ${props => props.color || Colors.Cyan};
+  flex-grow: 1;
+  font-size: 100%;
 `;
 
-export default props => (
-    <Container width={props.width}>
-        <Text {...props}>{props.label}</Text>
-    </Container>
-);
+export default class Key extends React.Component {
+  state = {
+    bgColor: null
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', this._onKeyPress);
+    document.addEventListener('keyup', this._onKeyRelease);
+  }
+
+  componentWillMount() {
+    document.removeEventListener('keydown', this._onKeyPress);
+    document.removeEventListener('keyup', this._onKeyRelease);
+  }
+
+  render() {
+    return (
+      <Container width={this.props.width} bgColor={this.state.bgColor}>
+        <Text {...this.props}>{this.props.label}</Text>
+      </Container>
+    );
+  }
+
+  _onKeyPress = e => {
+    if (this.props.label && e.key.toLowerCase() === this.props.label.toLowerCase()) {
+      this.setState({ bgColor: Colors.HalfCyan });
+    }
+  }
+
+  _onKeyRelease = e => {
+    if (this.props.label && e.key.toLowerCase() === this.props.label.toLowerCase()) {
+      this.setState({ bgColor: null });
+    }
+  }
+}
--- src/Components/Feedback/Keyboard.js
+++ src/Components/Feedback/Keyboard.js
@@ -5,30 +5,30 @@ import Key from "./Key";
 // TODO: hook up colors
 
 export default () => (
-    <>
-        <KeyRow>
-            {'~1234567890_+'.split('').map(label => <Key key={label} label={label}/>)}
-            <Key label='Bksp' width={2.2}/>
-        </KeyRow>
-        <KeyRow>
-            <Key label='Tab' width={1.6}/>
-            {'QWERTYUIOP[]'.split('').map(label => <Key key={label} label={label}/>)}
-            <Key label='\' width={1.6}/>
-        </KeyRow>
-        <KeyRow>
-            <Key label='Caps' width={2}/>
-            {'ASDFGHJKL;\''.split('').map(label => <Key key={label} label={label}/>)}
-            <Key label='Enter' width={2.5}/>
-        </KeyRow>
-        <KeyRow>
-            <Key label='Shift' width={3}/>
-            {'ZXCVBNM<>?'.split('').map(label => <Key key={label} label={label}/>)}
-            <Key label='Shift' width={3}/>
-        </KeyRow>
-        <KeyRow>
-            {'Ctrl Alt'.split(' ').map(label => <Key key={label} label={label}/>)}
-            <Key width={7}/>
-            {'Alt Ctrl'.split(' ').map(label => <Key key={label} label={label}/>)}
-        </KeyRow>
-    </>
+  <>
+    <KeyRow>
+      {'`1234567890-='.split('').map(label => <Key key={label} label={label} />)}
+      <Key width={2.2} />
+    </KeyRow>
+    <KeyRow>
+      <Key width={1.6} />
+      {'QWERTYUIOP[]'.split('').map(label => <Key key={label} label={label} />)}
+      <Key label='\' width={1.6} />
+    </KeyRow>
+    <KeyRow>
+      <Key width={2} />
+      {'ASDFGHJKL;\''.split('').map(label => <Key key={label} label={label} />)}
+      <Key label='Enter' width={2.5} />
+    </KeyRow>
+    <KeyRow>
+      <Key width={3} />
+      {'ZXCVBNM,./'.split('').map(label => <Key key={label} label={label} />)}
+      <Key width={3} />
+    </KeyRow>
+    <KeyRow>
+      {new Array(3).fill('').map((_, idx) => <Key key={idx} />)}
+      <Key label=' ' width={6} />
+      {new Array(3).fill('').map((_, idx) => <Key key={idx} />)}
+    </KeyRow>
+  </>
 );
